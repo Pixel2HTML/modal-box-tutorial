1381739b9a8036986fb254f6b53d7e16b9e88545
Miguel Palau <mpalauzarza@gmail.com>
2016-06-22 17:09:31 -0500

    Update 2016-06-22T22:09:26.833Z

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 (function() {
   (function($) {
-    var close, header, modal, trigger;
+    var close, closing, everything, header, modal, trigger;
     header = $('header');
     modal = $('.modal-box');
     trigger = $('header .modal-trigger');
     close = $('.modal-box .close');
+    everything = $(document);
     trigger.on('click', function() {
       header.addClass('is-blurred');
       return modal.addClass('is-active');
     });
-    return close.on('click', function() {
+    closing = function() {
       header.removeClass('is-blurred');
       return modal.removeClass('is-active');
+    };
+    close.on('click', closing);
+    return everything.keyup(function(e) {
+      if (e.keyCode === 27) {
+        return closing();
+      }
     });
   })(jQuery);
 
